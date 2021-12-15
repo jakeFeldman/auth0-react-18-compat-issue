e5489593d6e069c8c69afbb0382199f223cb1a4c
jakeFeldman <jacob.h.feldman@gmail.com>
2021-12-14 20:23:18 -0700

    repro repo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
+import createAuth0Client from "@auth0/auth0-spa-js";
+import React,  { useEffect, useState }  from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, useHistory } from "react-router-dom";
 
+import './index.css';
+
+function App() {
+  const [, setAuth0] = useState();
+  const history = useHistory();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const client = await createAuth0Client({
+          audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+          client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
+          domain:  process.env.REACT_APP_AUTH0_DOMAIN,
+          redirect_uri: window.location.origin
+        });
+        setAuth0(client);
+        if (
+          window.location.search.includes("code=") &&
+          window.location.search.includes("state=")
+        ) {
+          await client.handleRedirectCallback();
+          history.push(window.location.pathname)
+        }
+        const authed = await client.isAuthenticated();
+        if (authed) {
+          const [currentUser, token] = await Promise.all([
+            client.getUser(),
+            client.getTokenSilently()
+          ]);
+          console.log('currentUser, token', { currentUser, token });
+        } else {
+          await client.loginWithRedirect()
+        }
+      } catch (error) {
+        console.error('error', error);
+      }
+    })();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="App">
+      Test Auth App
+    </div>
+  );
+}
+
+// WORKS AS EXPECTED (i.e. React <= 17)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// THROWS AN ERROR (i.e. React >= 18)
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
